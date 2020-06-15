de85fa86c93fcdeb59f9ba04954018628f035c48
Chuck Muckamuck (pluto) <chuck.muckamuck@gmail.com>
2020-06-15 12:52:06 -0500

    Bump version: 0.1.0 → 0.2.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name="S3bkt",
-    version='0.1.0',
+    version='0.2.0',
     packages=['s3bkt'],
     description='S3 bucket utility',
     author='Chuck Muckamuck',
